4d50cc68025f15e7fd6abc05a0de7b33bb006b36
aimdexter <draimade@gmail.com>
2023-12-27 02:10:44 +0100

    feat: added readme and updated endpoints format

--- src/main/java/com/bookStore/controller/BookController.java
+++ src/main/java/com/bookStore/controller/BookController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/books")
+@RequestMapping("/api/v1/books")
 public class BookController {
 
 	@Autowired
